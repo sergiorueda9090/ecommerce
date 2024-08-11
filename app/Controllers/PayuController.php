29e2927ff54ee36ebb6cafc96666988671ad9d52
sergio rueda <sergiorueda90@hotmail.com>
2024-08-11 07:35:04 -0500

    Logs

--- app/Controllers/PayuController.php
+++ app/Controllers/PayuController.php
@@ -8,7 +8,17 @@ class PayuController extends BaseController{
     public function index(){
        
         #http://ecommerce/public/payuconfirmation?&payu=true&productos=8-3-2-1,8-2-2-1,8-1-2-1&merchantId=508029&merchant_name=Test+PayU+Test&merchant_address=Av+123+Calle+12&telephone=7512354&merchant_url=http%3A%2F%2Fpruebaslapv.xtrweb.com&transactionState=6&lapTransactionState=DECLINED&message=Declinada&referenceCode=2015-05-27+13%3A04%3A37&reference_pol=7069375&transactionId=f5e668f1-7ecc-4b83-a4d1-0aaa68260862&description=test_payu_01&trazabilityCode=&cus=&orderLanguage=es&extra1=&extra2=&extra3=&polTransactionState=6&signature=e1b0939bbdc99ea84387bee9b90e4f5c&polResponseCode=5&lapResponseCode=ENTITY_DECLINED&risk=1.00&polPaymentMethod=10&lapPaymentMethod=VISA&polPaymentMethodType=2&lapPaymentMethodType=CREDIT_CARD&installmentsNumber=1&TX_VALUE=100.00&TX_TAX=.00&currency=USD&lng=es&pseCycle=&buyerEmail=test%40payulatam.com&pseBank=&pseReference1=&pseReference2=&pseReference3=&authorizationCode=&TX_ADMINISTRATIVE_FEE=.00&TX_TAX_ADMINISTRATIVE_FEE=.00&TX_TAX_ADMINISTRATIVE_FEE_RETURN_BASE=.00
+        #https://ecommerce.sergiodevsolutions.com/payuconfirmation?&payu=true&productos=2-1-2-1
+
+          // Capturar toda la entrada que PayU envía
+          $inputData = file_get_contents('php://input');
         
+          // Cargar el servicio de logger
+          $logger = \Config\Services::logger();
+          
+          // Registrar la respuesta en el log
+          $logger->info('PayU Confirmation Response: ' . $inputData);
+          return;
         $session            = \Config\Services::session();
         $session            = session();
         $emailUser          = $session->get('email');
