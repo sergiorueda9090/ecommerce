0c5d075e9af9661e6d88001d83f20ac3d4eb07e4
sergio rueda <sergiorueda90@hotmail.com>
2024-08-11 09:08:21 -0500

    Fix

--- app/Controllers/PayuController.php
+++ app/Controllers/PayuController.php
@@ -7,25 +7,14 @@ class PayuController extends BaseController{
 
     public function index()
     {
-        // Escribir un log personalizado al iniciar el controlador
-        file_put_contents(WRITEPATH . 'logs/payu_custom_log.log', "Logging directly to file at start of index method" . PHP_EOL, FILE_APPEND);
-        
-        // Capturar los parámetros GET
+        file_put_contents(WRITEPATH . 'logs/payu_custom_log.log', "Start of index method" . PHP_EOL, FILE_APPEND);
+    
         $params = $this->request->getGet();
+        file_put_contents(WRITEPATH . 'logs/payu_custom_log.log', "Captured params: " . print_r($params, true) . PHP_EOL, FILE_APPEND);
+    
+        // Aquí puedes continuar con el resto de tu lógica.
         
-        // Registrar los parámetros en el log personalizado
-        $logMessage = "PayU Transaction Response:\n";
-        $logMessage .= "Merchant ID: " . ($params['merchantId'] ?? 'N/A') . "\n";
-        $logMessage .= "Reference Code: " . ($params['referenceCode'] ?? 'N/A') . "\n";
-        // Añadir más detalles...
-        
-        // Escribir los detalles de la transacción en el archivo de log personalizado
-        file_put_contents(WRITEPATH . 'logs/payu_custom_log.log', $logMessage . PHP_EOL, FILE_APPEND);
-        
-        // Escribir un log al final del procesamiento
-        file_put_contents(WRITEPATH . 'logs/payu_custom_log.log', "Finished logging transaction details" . PHP_EOL, FILE_APPEND);
-        
-        // Puedes continuar con cualquier otra lógica que necesites después
+        file_put_contents(WRITEPATH . 'logs/payu_custom_log.log', "End of index method" . PHP_EOL, FILE_APPEND);
     }
 
 }
