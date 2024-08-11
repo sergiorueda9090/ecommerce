af0874ce833dbbe884f8284055a2c60409e8366c
sergio rueda <sergiorueda90@hotmail.com>
2024-08-11 08:53:46 -0500

    as

--- app/Controllers/PayuController.php
+++ app/Controllers/PayuController.php
@@ -5,25 +5,27 @@ use App\Models\OrdersModel;
 
 class PayuController extends BaseController{
 
-    public function index(){
-       
-        #http://ecommerce/public/payuconfirmation?&payu=true&productos=8-3-2-1,8-2-2-1,8-1-2-1&merchantId=508029&merchant_name=Test+PayU+Test&merchant_address=Av+123+Calle+12&telephone=7512354&merchant_url=http%3A%2F%2Fpruebaslapv.xtrweb.com&transactionState=6&lapTransactionState=DECLINED&message=Declinada&referenceCode=2015-05-27+13%3A04%3A37&reference_pol=7069375&transactionId=f5e668f1-7ecc-4b83-a4d1-0aaa68260862&description=test_payu_01&trazabilityCode=&cus=&orderLanguage=es&extra1=&extra2=&extra3=&polTransactionState=6&signature=e1b0939bbdc99ea84387bee9b90e4f5c&polResponseCode=5&lapResponseCode=ENTITY_DECLINED&risk=1.00&polPaymentMethod=10&lapPaymentMethod=VISA&polPaymentMethodType=2&lapPaymentMethodType=CREDIT_CARD&installmentsNumber=1&TX_VALUE=100.00&TX_TAX=.00&currency=USD&lng=es&pseCycle=&buyerEmail=test%40payulatam.com&pseBank=&pseReference1=&pseReference2=&pseReference3=&authorizationCode=&TX_ADMINISTRATIVE_FEE=.00&TX_TAX_ADMINISTRATIVE_FEE=.00&TX_TAX_ADMINISTRATIVE_FEE_RETURN_BASE=.00
-        #https://ecommerce.sergiodevsolutions.com/payuconfirmation?&merchantId=508029&merchant_name=Test+PayU+Test&merchant_address=Av+123+Calle+12&telephone=7512354&merchant_url=http%3A%2F%2Fpruebaslapv.xtrweb.com&transactionState=6&lapTransactionState=DECLINED&message=Declinada&referenceCode=2015-05-27+13%3A04%3A37&reference_pol=7069375&transactionId=f5e668f1-7ecc-4b83-a4d1-0aaa68260862&description=test_payu_01&trazabilityCode=&cus=&orderLanguage=es&extra1=&extra2=&extra3=&polTransactionState=6&signature=e1b0939bbdc99ea84387bee9b90e4f5c&polResponseCode=5&lapResponseCode=ENTITY_DECLINED&risk=1.00&polPaymentMethod=10&lapPaymentMethod=VISA&polPaymentMethodType=2&lapPaymentMethodType=CREDIT_CARD&installmentsNumber=1&TX_VALUE=100.00&TX_TAX=.00&currency=USD&lng=es&pseCycle=&buyerEmail=test%40payulatam.com&pseBank=&pseReference1=&pseReference2=&pseReference3=&authorizationCode=&TX_ADMINISTRATIVE_FEE=.00&TX_TAX_ADMINISTRATIVE_FEE=.00&TX_TAX_ADMINISTRATIVE_FEE_RETURN_BASE=.00
-
-        #https://ecommerce.sergiodevsolutions.com/payuconfirmation?&payu=true&productos=2-1-2-1
-
-        $logger = \Config\Services::logger();
-
-        // Prueba de log simple
-        $logger->info('Logging in PayUController has started.');
-
-
-        // Crear un log bien formateado
+    public function index()
+    {
+        // Escribir un log personalizado al iniciar el controlador
+        file_put_contents(WRITEPATH . 'logs/payu_custom_log.log', "Logging directly to file at start of index method" . PHP_EOL, FILE_APPEND);
+        
+        // Capturar los parámetros GET
+        $params = $this->request->getGet();
+        
+        // Registrar los parámetros en el log personalizado
         $logMessage = "PayU Transaction Response:\n";
-
-
-        // Registrar en el log
-        $logger->info($logMessage);
+        $logMessage .= "Merchant ID: " . ($params['merchantId'] ?? 'N/A') . "\n";
+        $logMessage .= "Reference Code: " . ($params['referenceCode'] ?? 'N/A') . "\n";
+        // Añadir más detalles...
+        
+        // Escribir los detalles de la transacción en el archivo de log personalizado
+        file_put_contents(WRITEPATH . 'logs/payu_custom_log.log', $logMessage . PHP_EOL, FILE_APPEND);
+        
+        // Escribir un log al final del procesamiento
+        file_put_contents(WRITEPATH . 'logs/payu_custom_log.log', "Finished logging transaction details" . PHP_EOL, FILE_APPEND);
+        
+        // Puedes continuar con cualquier otra lógica que necesites después
     }
 
 }
